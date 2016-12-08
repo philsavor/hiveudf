839457effaf7693d621329bd3c9cb32d348caad9
Shen Xiangyu <shenxiangyu@qiyi.com>
2016-12-08 17:25:01 +0800

    ok

--- src/main/java/sample/hive/udf/Strip.java
+++ src/main/java/sample/hive/udf/Strip.java
@@ -7,12 +7,29 @@ import org.apache.hadoop.io.Text;
 public class Strip extends UDF {
   private Text result = new Text();
   
-  public Text evaluate(Text str) {
-    if (str == null) {
+  public Text evaluate(Text p1, Text contentid) {
+    if (p1 == null || contentid == null) {
       return null;
     }
-    result.set(StringUtils.strip(str.toString()));
-    result.set("test Result!!!");
+    
+    StringBuilder builder = new StringBuilder();
+    //f1: iphone
+    if("2_22_235".equals(p1))
+    	builder.append("1:1 ");
+    else
+    	builder.append("1:0 ");
+    
+    
+    //f2: android
+    if("2_22_236".equals(p1))
+    	builder.append("2:1 ");
+    else
+    	builder.append("2:0 ");
+    
+    //f3: contentid
+    if(null != contentid)
+    	builder.append("3:" + contentid + " ");
+    
     return result;
   }
   
