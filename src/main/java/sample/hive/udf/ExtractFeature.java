6ecb623099c7c1375936fa95fa929c591783a724
Shen Xiangyu <shenxiangyu@qiyi.com>
2016-12-12 10:53:50 +0800

    ok

--- src/main/java/sample/hive/udf/ExtractFeature.java
+++ src/main/java/sample/hive/udf/ExtractFeature.java
@@ -1,7 +1,10 @@
 package sample.hive.udf;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
@@ -12,22 +15,22 @@ public class ExtractFeature extends UDF {
   //cls
   private String cls;
   //stime
-  private String f_stime_0_6,f_stime_0_6_seq = "0";
-  private String f_stime_6_9,f_stime_6_9_seq = "1";
-  private String f_stime_9_12,f_stime_9_12_seq = "2";
-  private String f_stime_12_15,f_stime_12_15_seq = "3";
-  private String f_stime_15_18,f_stime_15_18_seq = "4";
-  private String f_stime_18_23,f_stime_18_23_seq = "5";
-  private String f_stime_other,f_stime_other_seq = "6";
+  private Feature fStime0To6 = new Feature(0);
+  private Feature fStime6To9 = new Feature(1);
+  private Feature fStime9To12 = new Feature(2);
+  private Feature fStime12To15 = new Feature(3);
+  private Feature fStime15To18 = new Feature(4);
+  private Feature fStime18To23 = new Feature(5);
+  private Feature fStimeOther = new Feature(6);
   //p1
-  private String f_p1_iphone,f_p1_iphone_seq = "7";
-  private String f_p1_android,f_p1_android_seq = "8";
+  private Feature fP1IPhone = new Feature(7);
+  private Feature fP1Android = new Feature(8);
   //net_work
-  private String f_net_work_4g,f_net_work_4g_seq = "9";
-  private String f_net_work_3g,f_net_work_3g_seq = "10";
-  private String f_net_work_2g,f_net_work_2g_seq = "11";
-  private String f_net_work_wifi,f_net_work_wifi_seq = "12";
-  private String f_net_work_other,f_net_work_other_seq = "13";
+  private Feature fNetwork4g = new Feature(9);
+  private Feature fNetwork3g = new Feature(10);
+  private Feature fNetwork2g = new Feature(11);
+  private Feature fNetworkWifi = new Feature(12);
+  private Feature fNetworkOther = new Feature(13);
   
   public Text evaluate( Text action
 		  				,Text stime
@@ -63,13 +66,13 @@ public class ExtractFeature extends UDF {
     	cls = "0";
     
     /*
-     * f_stime_0_6
-     * f_stime_6_9
-     * f_stime_9_12
-     * f_stime_12_15
-     * f_stime_15_18
-     * f_stime_18_23
-     * f_stime_other
+     * fStime0To6
+     * fStime6To9
+     * fStime9To12
+     * fStime12To15
+     * fStime15To18
+     * fStime18To23
+     * fStimeOther
      */
     if(null != stime){
 		Long timestamp = Long.parseLong(stime.toString());
@@ -78,114 +81,110 @@ public class ExtractFeature extends UDF {
 		int hour = Calendar.HOUR;
 		switch(hour){
 			case 0: case 1: case 2: case 3: case 4: case 5: case 6:
-				f_stime_0_6 = "1";
-				f_stime_6_9 = "0";
-				f_stime_9_12 = "0";
-				f_stime_12_15 = "0";
-				f_stime_15_18 = "0";
-				f_stime_18_23 = "0";
-				f_stime_other = "0";
+				fStime0To6.setValue("1");
+				fStime6To9.setValue("0");
+				fStime9To12.setValue("0");
+				fStime12To15.setValue("0");
+				fStime15To18.setValue("0");
+				fStime18To23.setValue("0");
+				fStimeOther.setValue("0");
 				break;
 			case 7: case 8: case 9: 
-				f_stime_0_6 = "0";
-				f_stime_6_9 = "1";
-				f_stime_9_12 = "0";
-				f_stime_12_15 = "0";
-				f_stime_15_18 = "0";
-				f_stime_18_23 = "0";
-				f_stime_other = "0";
+				fStime0To6.setValue("0");
+				fStime6To9.setValue("1");
+				fStime9To12.setValue("0");
+				fStime12To15.setValue("0");
+				fStime15To18.setValue("0");
+				fStime18To23.setValue("0");
+				fStimeOther.setValue("0");
 				break;
 			case 10: case 11: case 12: 
-				f_stime_0_6 = "0";
-				f_stime_6_9 = "0";
-				f_stime_9_12 = "1";
-				f_stime_12_15 = "0";
-				f_stime_15_18 = "0";
-				f_stime_18_23 = "0";
-				f_stime_other = "0";
+				fStime0To6.setValue("0");
+				fStime6To9.setValue("0");
+				fStime9To12.setValue("1");
+				fStime12To15.setValue("0");
+				fStime15To18.setValue("0");
+				fStime18To23.setValue("0");
+				fStimeOther.setValue("0");
 				break;
 			case 13: case 14: case 15: 
-				f_stime_0_6 = "0";
-				f_stime_6_9 = "0";
-				f_stime_9_12 = "0";
-				f_stime_12_15 = "1";
-				f_stime_15_18 = "0";
-				f_stime_18_23 = "0";
-				f_stime_other = "0";
+				fStime0To6.setValue("0");
+				fStime6To9.setValue("0");
+				fStime9To12.setValue("0");
+				fStime12To15.setValue("1");
+				fStime15To18.setValue("0");
+				fStime18To23.setValue("0");
+				fStimeOther.setValue("0");
 				break;
 			case 16: case 17: case 18: 
-				f_stime_0_6 = "0";
-				f_stime_6_9 = "0";
-				f_stime_9_12 = "0";
-				f_stime_12_15 = "0";
-				f_stime_15_18 = "1";
-				f_stime_18_23 = "0";
-				f_stime_other = "0";
+				fStime0To6.setValue("0");
+				fStime6To9.setValue("0");
+				fStime9To12.setValue("0");
+				fStime12To15.setValue("0");
+				fStime15To18.setValue("1");
+				fStime18To23.setValue("0");
+				fStimeOther.setValue("0");
 				break;
 			case 19: case 20: case 21: case 22: case 23:
-				f_stime_0_6 = "0";
-				f_stime_6_9 = "0";
-				f_stime_9_12 = "0";
-				f_stime_12_15 = "0";
-				f_stime_15_18 = "0";
-				f_stime_18_23 = "1";
-				f_stime_other = "0";
+				fStime0To6.setValue("0");
+				fStime6To9.setValue("0");
+				fStime9To12.setValue("0");
+				fStime12To15.setValue("0");
+				fStime15To18.setValue("0");
+				fStime18To23.setValue("1");
+				fStimeOther.setValue("0");
 				break;
 			default:
-				f_stime_0_6 = "0";
-				f_stime_6_9 = "0";
-				f_stime_9_12 = "0";
-				f_stime_12_15 = "0";
-				f_stime_15_18 = "0";
-				f_stime_18_23 = "0";
-				f_stime_other = "1";
+				fStime0To6.setValue("0");
+				fStime6To9.setValue("0");
+				fStime9To12.setValue("0");
+				fStime12To15.setValue("0");
+				fStime15To18.setValue("0");
+				fStime18To23.setValue("0");
+				fStimeOther.setValue("1");
 				break;
 		}
     }else{
-		f_stime_0_6 = "0";
-		f_stime_6_9 = "0";
-		f_stime_9_12 = "0";
-		f_stime_12_15 = "0";
-		f_stime_15_18 = "0";
-		f_stime_18_23 = "0";
-		f_stime_other = "1";
+		fStime0To6.setValue("0");
+		fStime6To9.setValue("0");
+		fStime9To12.setValue("0");
+		fStime12To15.setValue("0");
+		fStime15To18.setValue("0");
+		fStime18To23.setValue("0");
+		fStimeOther.setValue("1");
     }
     
     /*
-     * f_p1_iphone
+     * fP1IPhone
+     * fP1Android
      */
     String p1String = p1.toString();
-    
     if(ExtractFeatureConstant.DATA_P1_IPHONE.equals(p1String))
-    	f_p1_iphone = "1";
+    	fP1IPhone.setValue("1");
     else
-    	f_p1_iphone = "0";
+    	fP1IPhone.setValue("0");
     
-    
-    /*
-     * f_p1_android
-     */
     if(ExtractFeatureConstant.DATA_P1_ANDROID.equals(p1String))
-    	f_p1_android = "1";
+    	fP1Android.setValue("1");
     else
-    	f_p1_android = "0";
+    	fP1Android.setValue("0");
     
     /*
-     * f_net_work_4g
-     * f_net_work_3g
-     * f_net_work_2g
-     * f_net_work_wifi
-     * f_net_work_other
+     * fNetwork4g
+     * fNetwork3g
+     * fNetwork2g
+     * fNetworkWifi
+     * fNetworkOther
      */
     if(null != net_work){
 		String netWorkString = net_work.toString();
 		switch(netWorkString){
 			case ExtractFeatureConstant.DATA_NETWORK_LTE:
-				f_net_work_4g = "1";
-				f_net_work_3g = "0";
-				f_net_work_2g = "0";
-				f_net_work_wifi = "0";
-				f_net_work_other = "0";
+				fNetwork4g.setValue("1");
+				fNetwork3g.setValue("0");
+				fNetwork2g.setValue("0");
+				fNetworkWifi.setValue("0");
+				fNetworkOther.setValue("0");
 				break;
 			case ExtractFeatureConstant.DATA_NETWORK_UMTS:
 			case ExtractFeatureConstant.DATA_NETWORK_HSDPA:
@@ -195,76 +194,83 @@ public class ExtractFeature extends UDF {
 			case ExtractFeatureConstant.DATA_NETWORK_EVDO_0:
 			case ExtractFeatureConstant.DATA_NETWORK_EVDO_A:
 			case ExtractFeatureConstant.DATA_NETWORK_HSPAP:
-				f_net_work_4g = "0";
-				f_net_work_3g = "1";
-				f_net_work_2g = "0";
-				f_net_work_wifi = "0";
-				f_net_work_other = "0";
+				fNetwork4g.setValue("0");
+				fNetwork3g.setValue("1");
+				fNetwork2g.setValue("0");
+				fNetworkWifi.setValue("0");
+				fNetworkOther.setValue("0");
 				break;
 			case ExtractFeatureConstant.DATA_NETWORK_GPRS:
 			case ExtractFeatureConstant.DATA_NETWORK_EDGE:
 			case ExtractFeatureConstant.DATA_NETWORK_1XRTT:
-				f_net_work_4g = "0";
-				f_net_work_3g = "0";
-				f_net_work_2g = "1";
-				f_net_work_wifi = "0";
-				f_net_work_other = "0";
+				fNetwork4g.setValue("0");
+				fNetwork3g.setValue("0");
+				fNetwork2g.setValue("1");
+				fNetworkWifi.setValue("0");
+				fNetworkOther.setValue("0");
 				break;
 			case ExtractFeatureConstant.DATA_NETWORK_WIFI:
 			case ExtractFeatureConstant.DATA_NETWORK_ETHERNET:
-				f_net_work_4g = "0";
-				f_net_work_3g = "0";
-				f_net_work_2g = "0";
-				f_net_work_wifi = "1";
-				f_net_work_other = "0";
+				fNetwork4g.setValue("0");
+				fNetwork3g.setValue("0");
+				fNetwork2g.setValue("0");
+				fNetworkWifi.setValue("1");
+				fNetworkOther.setValue("0");
 				break;
 			case ExtractFeatureConstant.DATA_NETWORK_NONETWORK_ANDROID:
 			case ExtractFeatureConstant.DATA_NETWORK_NONETWORK:
-				f_net_work_4g = "0";
-				f_net_work_3g = "0";
-				f_net_work_2g = "0";
-				f_net_work_wifi = "0";
-				f_net_work_other = "1";
+				fNetwork4g.setValue("0");
+				fNetwork3g.setValue("0");
+				fNetwork2g.setValue("0");
+				fNetworkWifi.setValue("0");
+				fNetworkOther.setValue("1");
 				break;
 			default:
-				f_net_work_4g = "0";
-				f_net_work_3g = "0";
-				f_net_work_2g = "0";
-				f_net_work_wifi = "0";
-				f_net_work_other = "1";
+				fNetwork4g.setValue("0");
+				fNetwork3g.setValue("0");
+				fNetwork2g.setValue("0");
+				fNetworkWifi.setValue("0");
+				fNetworkOther.setValue("1");
 				break;
 		}
 	}else{
-		f_net_work_4g = "0";
-		f_net_work_3g = "0";
-		f_net_work_2g = "0";
-		f_net_work_wifi = "0";
-		f_net_work_other = "1";
+		fNetwork4g.setValue("0");
+		fNetwork3g.setValue("0");
+		fNetwork2g.setValue("0");
+		fNetworkWifi.setValue("0");
+		fNetworkOther.setValue("1");
     }
     
     /*
      *	setup features' sequence
      */
+    List<Feature> featureList = new ArrayList<Feature>();
+    featureList.add(fStime0To6);
+    featureList.add(fStime6To9);
+    featureList.add(fStime9To12);
+    featureList.add(fStime12To15);
+    featureList.add(fStime15To18);
+    featureList.add(fStime18To23);
+    featureList.add(fStimeOther);
+    featureList.add(fP1IPhone);
+    featureList.add(fP1Android);
+    featureList.add(fNetwork4g);
+    featureList.add(fNetwork3g);
+    featureList.add(fNetwork2g);
+    featureList.add(fNetworkWifi);
+    featureList.add(fNetworkOther);
+    Collections.sort(featureList);
+    
     builder.append(cls + " "); 
-    builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_stime_0_6_seq, f_stime_0_6) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_stime_6_9_seq, f_stime_6_9) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_stime_9_12_seq, f_stime_9_12) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_stime_12_15_seq, f_stime_12_15) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_stime_15_18_seq, f_stime_15_18) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_stime_18_23_seq, f_stime_18_23) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_stime_other_seq, f_stime_other) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_p1_iphone_seq, f_p1_iphone) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_p1_android_seq, f_p1_android) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_net_work_4g_seq, f_net_work_4g) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_net_work_3g_seq, f_net_work_3g) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_net_work_2g_seq, f_net_work_2g) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_net_work_wifi_seq, f_net_work_wifi) + " ");
-	builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f_net_work_other_seq, f_net_work_other));
+    for(Feature f : featureList){
+    	if(f.getValue() != null)
+    		builder.append(MessageFormat.format(ExtractFeatureConstant.FEATURE_STRING_FORMAT, f.getIndex(), f.getValue()) + " ");
+    }
     /*
      *	setup features' sequence
      */
     
-    result.set(builder.toString());
+    result.set(builder.toString().trim());
     
     return result;
   }
